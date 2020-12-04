bf7d1c7b715c12afcb405acc2ea8fe474ff640fb
Kosuke Kamimori <kamimori.kosuke@fixer.co.jp>
2020-12-04 15:39:00 +0900

    Delete CheckAADGuestStatus_config.ps1

--- CheckAADGuestStatus_config.ps1
+++
@@ -1,9 +0,0 @@
-﻿#各種Init
-#ループ時間（秒単位）
-$global:waitSecond_default = 60
-
-#WebhookのURL(Slack API[https://api.slack.com/apps/{AppID}/incoming-webhooks]から取得)
-$global:Webhook_default = ""
-
-#通知に利用するアイコンのURI(任意)
-$global:image_default = ""
